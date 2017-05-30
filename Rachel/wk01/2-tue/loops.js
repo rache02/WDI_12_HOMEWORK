58d35f102fd146567cecd227d42e14ba4aefab87
rachel <rache882@gmail.com>
2017-05-31 00:42:28 +1000

    homework inprogress

---
+++ Rachel/wk01/2-tue/loops.js
@@ -0,0 +1,30 @@
+console.log('Homework Loops');
+
+// The Even/Odd Reporter
+// for (var count = 1; count <= 20; count++) {
+//   if (count % 2 === 0) {
+//     console.log(count + ' is even');
+//   } else {
+//     console.log(count + ' is odd');
+//   }
+// }
+
+// Multiplication Tables
+// for (var count = 0;  count <= 10; count++) {
+//   console.log(count + ' * 9 = ' + count*9);
+// }
+
+// Your Top Choices
+var myfavoritecolor = ['red','green','blue','orange'];
+  alert(myfavoritecolor[2]);
+  
+
+// // JS else if statements
+// var year = Number;
+//   if (year === 2015) {
+//     console.log('Im in the present!');
+//   } else if (year < 2015) {
+//     console.log('Whoa! Blast from the past!');
+//   } else if (year > 2015) {
+//     console.log('Greetings from the future!');
+//   }
